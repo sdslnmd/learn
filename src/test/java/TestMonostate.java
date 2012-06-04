06f842971d736a926f29d4fd23902f013ddad119
sunluning <sdslnmd@gmail.com>
2012-06-04 23:52:46 +0800

    First commit

---
+++ src/test/java/TestMonostate.java
@@ -0,0 +1,24 @@
+import junit.framework.TestCase;
+
+public class TestMonostate extends TestCase {
+    public TestMonostate(String name) {
+        super(name);
+    }
+
+    public void testInstance() {
+        Monostate m = new Monostate();
+        for (int x = 0; x < 10; x++) {
+            m.setX(x);
+            assertEquals(x, m.getX());
+        }
+    }
+
+    public void testInstanceBehaveAsOne() {
+        Monostate m1 = new Monostate();
+        Monostate m2 = new Monostate();
+        for (int x = 0; x < 10; x++) {
+            m1.setX(x);
+            assertEquals(x, m2.getX());
+        }
+    }
+}
