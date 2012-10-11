c4331f44a3b9880d1886ac4e8552a4a70c514643
sunluning <sdslnmd@gmail.com>
2012-10-12 07:56:15 +0800

    add sort rule

--- trick/src/main/java/com/engineer/sun/sort/PersonComparator.java
+++ trick/src/main/java/com/engineer/sun/sort/PersonComparator.java
@@ -1,9 +1,9 @@
 package com.engineer.sun.sort;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.text.Collator;
+import java.text.ParseException;
+import java.text.RuleBasedCollator;
+import java.util.*;
 
 enum PersonComparator implements Comparator<Person> {
     ID_SORT {
@@ -15,7 +15,25 @@ enum PersonComparator implements Comparator<Person> {
         public int compare(Person o1, Person o2) {
             return o1.getName().compareTo(o2.getName());
         }
-    };
+    },
+    RULE_SORT {
+        public int compare(Person o1, Person o2) {
+            try {
+                RuleBasedCollator rule = new RuleBasedCollator("< '砸人' <'发哥' < '阿门' ");
+                return rule.compare(o1.getName(),o2.getName());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return o1.getName().compareTo(o2.getName());
+        }
+    },
+    CHINA_SORT {
+        public int compare(Person o1, Person o2) {
+            RuleBasedCollator collator_ch = (RuleBasedCollator) Collator.getInstance(Locale.CHINA);
+            return collator_ch.compare(o1.getName(),o2.getName());
+        }
+    },
+    ;
 
     public static Comparator<Person> desc(final Comparator<Person> other) {
         return new Comparator<Person>() {
@@ -50,14 +68,20 @@ enum PersonComparator implements Comparator<Person> {
 
     public static void main(String[] args) {
         List<Person> list = new ArrayList<Person>();
-        list.add(new Person("beijing",1,"b"));
-        list.add(new Person("shanghai",2,"a"));
-        list.add(new Person("shanghai",1,"a"));
-        list.add(new Person("guangdong",3,"c"));
-        Collections.sort(list, asc(getComparator(NAME_SORT, ID_SORT)));
+        list.add(new Person("beijing", 1, "白痴"));
+        list.add(new Person("haiyang", 2, "阿门"));
+        list.add(new Person("shanghai", 1, "砸人"));
+        list.add(new Person("guangdong", 3, "发哥"));
+
+        Locale loc = new Locale("sr", "RS");
+
+        Collator col = Collator.getInstance(loc);
+        col.setStrength(Collator.SECONDARY);
+
+        Collections.sort(list, asc(getComparator(RULE_SORT)));
 
         for (Person person : list) {
-            System.out.println(person.getName()+person.getId());
+            System.out.println(person.getAddress()+person.getName() + person.getId());
         }
     }
 }
