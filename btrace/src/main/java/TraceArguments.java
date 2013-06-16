3bfea3b6a3ba00a8cbd5c5413f4fb1f6dd3d356b
sunluning <sdslnmd@gmail.com>
2013-06-16 11:57:46 +0800

    add commit

--- btrace/src/main/java/TraceArguments.java
+++ btrace/src/main/java/TraceArguments.java
@@ -8,6 +8,10 @@ public class TraceArguments {
             method = "/.*/",
             location = @Location(value = Kind.RETURN,clazz = "/.*calc.*/",method = "/.*/")
     )
+    /*
+      参数的顺序需要和监控的方法名一样
+      @result需配置 kind.RETURN一起使用
+    * */
     public static void t(String name,@Return String result) {
         println(strcat("args = ", name));
         println(strcat("re = ", result));
