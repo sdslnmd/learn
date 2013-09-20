267e8a5a9a47799973632a1808cb090ddc209b8d
sunluning <sdslnmd@gmail.com>
2013-09-20 22:31:50 +0800

    add

--- btrace/src/main/java/TraceArguments.java
+++ btrace/src/main/java/TraceArguments.java
@@ -11,6 +11,10 @@ public class TraceArguments {
             method = "process"
             ,location = @Location(Kind.RETURN)
     )
+    /**
+     * 1 process(Name) 格式，将name作为参数至于m方法,然后利用field方法获取字段值
+     *
+     */
     public static void m(@ProbeMethodName String me,@Return Name re,Name name) {
 
         println(get(field("com.mkyong.common.controller.obj.Name", "name"), re));
