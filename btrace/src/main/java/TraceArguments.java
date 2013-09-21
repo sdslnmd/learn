78ac68a1f7e8856e6fffd335701216bd2982567a
sunluning <sdslnmd@gmail.com>
2013-09-21 12:01:42 +0800

    add

--- btrace/src/main/java/TraceArguments.java
+++ btrace/src/main/java/TraceArguments.java
@@ -12,8 +12,8 @@ public class TraceArguments {
             ,location = @Location(Kind.RETURN)
     )
     /**
-     * 1 process(Name) 格式，将name作为参数至于m方法,然后利用field方法获取字段值
-     * 2 查看target 中的字段名称,在行参中使用@self 标记onMethod中的class,然后利用field方法获取值
+     * 1 process(Name) 格式，将name作为参数至于m方法,然后利用field（filed,name）方法获取字段值
+     * 2 查看target 中的字段名称,在行参中使用@self 标记onMethod中的class,然后利用field(filed,self)方法获取值
      */
     public static void m(@ProbeMethodName String me,@Return Name re,Name name) {
 
