4243de88b4c930dec04de0c474f650bca7eaeba8
sunluning <sdslnmd@gmail.com>
2013-09-21 15:54:42 +0800

    add

--- btrace/src/main/java/TraceArguments.java
+++ btrace/src/main/java/TraceArguments.java
@@ -6,18 +6,16 @@ import static com.sun.btrace.BTraceUtils.*;
 @BTrace
 public class TraceArguments {
 
-    @OnMethod(
-            clazz = "com.mkyong.common.controller.obj.NameProcess",
-            method = "process"
-            ,location = @Location(Kind.RETURN)
-    )
-    /**
-     * 1 process(Name) 格式，将name作为参数至于m方法,然后利用field（filed,name）方法获取字段值
-     * 2 查看target 中的字段名称,在行参中使用@self 标记onMethod中的class,然后利用field(filed,self)方法获取值
-     */
-    public static void m(@ProbeMethodName String me,@Return Name re,Name name) {
+    // process(Name) 格式，将name作为参数至于m方法,然后利用field（filed,name）方法获取字段值
+    @OnMethod(clazz = "com.mkyong.common.controller.obj.NameProcess", method = "process")
+    public static void m(Name name) {
+        println(get(field("com.mkyong.common.controller.obj.Name", "name"), name));
 
-        println(get(field("com.mkyong.common.controller.obj.Name", "name"), re));
+    }
+
+    //查看target 中的字段名称,在行参中使用@self 标记onMethod中的class,然后利用field(filed,self)方法获取值
+    @OnMethod(clazz = "com.mkyong.common.controller.obj.Name", method = "getName")
+    public static void m1(@Self Name name) {
         println(get(field("com.mkyong.common.controller.obj.Name", "name"), name));
 
     }
