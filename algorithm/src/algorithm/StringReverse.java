accb9484d26836658a5671b13cf18d453770b3a0
sunluning <sdslnmd@gmail.com>
2013-12-26 23:13:24 +0800

    m

--- algorithm/src/algorithm/StringReverse.java
+++ algorithm/src/algorithm/StringReverse.java
@@ -1,4 +1,28 @@
 package algorithm;
 
 public class StringReverse {
+
+    public static String reverse(String str) {
+
+        char[] chars = str.toCharArray();
+
+        int i = 0;
+        int j = chars.length-1;
+
+        while (i < j) {
+            char tmp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = tmp;
+            i++;
+            j--;
+        }
+
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        String reverse = StringReverse.reverse("4321");
+        System.out.println(reverse);
+
+    }
 }
